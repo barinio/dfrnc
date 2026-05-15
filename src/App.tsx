30d343a2f334a815d0f89edb82828f7a8a78d25b
Ванька <7328652@gmail.com>
2026-05-15 18:20:23 +0300

    chore: rename main + App to .tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LottieOverlay from './components/LottieOverlay'
+import Scene from './components/Scene'
+
+export default function App() {
+  return (
+    <>
+      <LottieOverlay />
+      <Scene />
+    </>
+  )
+}
